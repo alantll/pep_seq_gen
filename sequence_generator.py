b6acfe9affaf1339142a5ec28eb549b33393c89e
alantll <altllee@gmail.com>
2022-10-01 22:18:54 -0400

    added exceptions

--- sequence_generator.py
+++ sequence_generator.py
@@ -1,47 +1,85 @@
 from itertools import product
-from aa_mass import aa_masses as aa
-from mass_calculator import calc_mass
+from molwts import molwts
+from molwt_calculator import calc_molwt
 
-def conjugates_to_list(conjugates):
+def string_to_list(input):
     """
-    Add peptide conjugates to list.
-    :params conjugates: string of peptide conjugates.
-    :return: list of conjugates.
+    Convert input string to list of peptide fragments.
+    :params input (str): string of peptide fragments (comma-separated).
+    :return: list of fragments and invalid inputs.
     """
-    #conj = [conj.strip() for conj in peptide.upper().split(',')]
-    conjugates_list = []
-    for conjugate in conjugates.upper().split(','):
-        conjugates_list.append(conjugate.strip())
-    conjugates_list.sort()
+    fragment_lst = []
+    invalid_lst = []
+    for fragment in input.upper().split(','):
+        for residue in fragment:
+            if residue not in molwts.keys():
+                invalid_lst.append(residue)
+            else:
+                if fragment not in fragment_lst:
+                    fragment_lst.append(fragment.strip())
+                else:
+                    pass
 
-    return conjugates_list
+    fragment_lst.sort()
+    invalid_lst.sort()
 
-def generate_sequences(conjugates_list, num_connects):
+    return fragment_lst, invalid_lst
+
+def generate_sequences(fragment_lst, max_connections, to_csv='n'):
     """
-    Generate all possible peptide sequences from fragments
-    :param conjugates_list: List of conjugates to combine.
-    :param num_connects: number of connections.
-    :return: None
+    Generate all possible peptide sequences from a list of fragments.
+    :param fragment_lst (list): List of amino acid or peptide fragments to combine.
+    :param max_connections (int): Maximum number of connections formed.
+    :param to_csv (str): Save data to file. Default is 'n'
+    :return: summary string and sequence string containing all possible sequences and the molecular weights.
     """
+    summary = ""
+    output = "Sequence, MW\n"
     total = 0
-    for i in range(2, int(num_connects) + 2):
+    for i in range(1, max_connections + 1):
         count = 0
-        for conjugates in product(conjugates_list, repeat=i):
-            sequences = ''.join(conjugates)
-            print(sequences, format(calc_mass(sequences), '.4f'))
+        for fragments in product(fragment_lst, repeat=i+1):
+            conjugate = ''.join(fragments)
+            output += f'{conjugate}, {calc_molwt(conjugate):.4f}\n'
             count += 1
             total += 1
-        print(f'{count} fragments with {i - 1} bond(s) formed')
-    print('Total number of possible fragments: ', total)
+        summary += f'>{count} possible products with {i} connections(s)\n'
+    summary = f'--Total number of possible products: {total}\n' + summary
 
-    return
+    if to_csv == 'y':
+        with open('data.csv', 'w') as out:
+            out.write(output)
+
+    return summary, output
 
 def main():
-    peptides = input('Enter peptide conjugates: ')
-    num_connect = input('Maximum number of bonds formed: ')
+    fragment_str = input('Amino acid/peptide fragments: ')
+    fragments, invalids = string_to_list(fragment_str)
+    if len(invalids) != 0:
+        print("Invalid amino acids: '{}'.".format("', '".join(invalids)))
 
-    conjugates = conjugates_to_list(peptides)
-    generate_sequences(conjugates, num_connect)
+        return
+
+    try:
+        max_connections = int(input('Maximum number of connections: '))
+    except ValueError:
+        print('Max connections requires a number.')
+
+        return
+
+    save_yn = input('Save to file? (y/n): ')
+    if save_yn not in 'yn':
+        print("Must provide 'y' or 'n' for save to file")
+
+        return
+
+    summary, results = generate_sequences(fragments, max_connections, to_csv=save_yn)
+    print("\nInput fragments: '{}'".format("', '".join(fragments)))
+    print(f'Maximum connections: {max_connections}')
+    print(summary)
+    print(results)
+
+    return
 
 if __name__ == "__main__":
     main()
